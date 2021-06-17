8b48c53e8de8f72e57ba9773501636bc27373d64
ZhangQR <ricey54560@gmail.com>
2021-06-17 23:58:55 +0800

    init

---
+++ Assets/02Graph/Graph.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField] GameObject point; 
+    [SerializeField] int solution; // 会产生多少个点
+
+    
+}
